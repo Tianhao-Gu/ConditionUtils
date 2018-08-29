5e1cb36bf26a5424965756550dc11d262dcb57fc
Tianhao-Gu <tgu@anl.gov>
2018-08-29 16:09:14 -0500

    revert client server python

--- lib/ConditionUtils/ConditionUtilsClient.py
+++ lib/ConditionUtils/ConditionUtilsClient.py
@@ -23,7 +23,7 @@ class ConditionUtils(object):
             self, url=None, timeout=30 * 60, user_id=None,
             password=None, token=None, ignore_authrc=False,
             trust_all_ssl_certificates=False,
-            auth_svc='https://kbase.us/services/authorization/Sessions/Login'):
+            auth_svc='https://ci.kbase.us/services/auth/api/legacy/KBase/Sessions/Login'):
         if url is None:
             raise ValueError('A url is required')
         self._service_ver = None
@@ -91,8 +91,8 @@ class ConditionUtils(object):
         :param params: instance of type "ExportConditionSetParams" ->
            structure: parameter "input_ref" of type "ws_condition_set_id"
            (@id ws KBaseExperiments.ConditionSet)
-        :returns: instance of type "ExportOutput" -> structure: parameter
-           "shock_id" of String
+        :returns: instance of type "ExportConditionSetOutput" -> structure:
+           parameter "shock_id" of String
         """
         return self._client.call_method(
             'ConditionUtils.export_condition_set_tsv',
@@ -103,23 +103,11 @@ class ConditionUtils(object):
         :param params: instance of type "ExportConditionSetParams" ->
            structure: parameter "input_ref" of type "ws_condition_set_id"
            (@id ws KBaseExperiments.ConditionSet)
-        :returns: instance of type "ExportOutput" -> structure: parameter
-           "shock_id" of String
+        :returns: instance of type "ExportConditionSetOutput" -> structure:
+           parameter "shock_id" of String
         """
         return self._client.call_method(
             'ConditionUtils.export_condition_set_excel',
-            [params], self._service_ver, context)
-
-    def export_cluster_set_excel(self, params, context=None):
-        """
-        :param params: instance of type "ExportClusterSetParams" ->
-           structure: parameter "input_ref" of type "ws_cluster_set_id" (@id
-           ws KBaseExperiments.ClusterSet)
-        :returns: instance of type "ExportOutput" -> structure: parameter
-           "shock_id" of String
-        """
-        return self._client.call_method(
-            'ConditionUtils.export_cluster_set_excel',
             [params], self._service_ver, context)
 
     def status(self, context=None):
